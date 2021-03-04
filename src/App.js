15b13e2e8f33f061721fc8fcc0e40f64236b0682
antoniorm80 <antoniorm80@hotmail.com>
2021-03-04 14:31:02 -0600

    Modificando los mensajes vacíos del Login Page

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         message.success('Pero, ¿Porqué lo tienes TODO, Bb?');
       }else{
         // alert('éallleeee!, capture los datos');
-        message.warning('éallleeee! captura tus datos Baby!');
+        message.warning('éallleeee! captura tus datos, Baby!');
       }
     } catch (error) {
       console.log(error.message)
