a68bc6f38f32e05400a86de67ad138b4717c27d6
antoniorm80 <antoniorm80@hotmail.com>
2021-03-06 14:56:08 -0600

    Modificando el Título de la Aplicación

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react'
   import './App.css';
 import 'antd/dist/antd.css'; 
-import { Form, Input, Button, Checkbox, message } from 'antd';
+import { Form, Input, Button, Checkbox, message, Alert } from 'antd';
 import { UserOutlined, LockOutlined, BarcodeOutlined } from '@ant-design/icons';
 
 
@@ -34,85 +34,72 @@ function App() {
 
   return (
     <div className="App-header">
+    <Alert message="Favor de capturar sus datos para continuar..." type="success" closeText="X" showIcon />,
     <h3>Punto de Venta</h3>
-    <BarcodeOutlined style={{ fontSize: '90px', color: '#000'}}/>
-    <br/>
-       {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> 
-      <DatePicker />*/}
+      <BarcodeOutlined style={{ fontSize: '90px', color: '#000'}}/>
+      <br/>
       <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        // rules={[
-        //   {
-        //     required: true,
-        //     message: 'Escriba su Usuario, por favor! ',
-        //   },
-        // ]}
-      >
-        <Input 
-          prefix={<UserOutlined 
-          className="site-form-item-icon" />} 
-          placeholder="Usuario" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)}
-          required
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+        <Form.Item
+          name="username"
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: 'Escriba su Usuario, por favor! ',
+          //   },
+          // ]}
+        >
+          <Input 
+            prefix={<UserOutlined 
+            className="site-form-item-icon" />} 
+            placeholder="Usuario" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)}
+            maxLength='15'
+            required
+            />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: 'Escriba su Contraseña, por favor! ',
+          //   },
+          // ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Contraseña"
+            value="password"
+            onChange={(e) => setPassword(e.target.value)}
+            maxLength='15'
+            required
           />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        // rules={[
-        //   {
-        //     required: true,
-        //     message: 'Escriba su Contraseña, por favor! ',
-        //   },
-        // ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Contraseña"
-          value="password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Recordarme</Checkbox>
+        </Form.Item>
+        <Form.Item >
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Recordar</Checkbox>
+          </Form.Item>
+           {/* <a className="login-form-forgot" href="https://www.google.com/" target="blank">
+            Olvidé mi Contraseña
+          </a>  */}
         </Form.Item>
 
-        <a className="login-form-forgot" href="https://www.google.com/" target="blank">
-          Olvidé mi Contraseña
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary"  className="login-form-button" onClick={handleSubmit}>
-          Accesar
-        </Button>
-         <a href="https://www.google.com/" target="blank" style={{ alignContent: 'center'}}>Registrarse!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button type="primary"  className="login-form-button" onClick={handleSubmit}>
+            Accesar
+          </Button>
+         {/* <a href="https://www.google.com/" target="blank" style={{ alignContent: 'center'}}>Registrarse!</a> */} 
+        </Form.Item>
+      </Form>
 
     </div>
   );
